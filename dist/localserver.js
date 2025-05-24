5c83ba31ac8dc90e7e95270613c81c259d64ffaa
VictorLeBgDu92 <129321237+VcrazYDev@users.noreply.github.com>
2025-05-24 08:05:06 +0200

    Update localserver.js

--- dist/localserver.js
+++ dist/localserver.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 let player;
 let server;
 
@@ -3054,25 +3042,3 @@ window.WebSocket = class {
         }
     }
 };
-
-}
-/*
-     FILE ARCHIVED ON 16:04:58 Jan 26, 2023 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 05:54:59 May 24, 2025.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.552
-  exclusion.robots: 0.022
-  exclusion.robots.policy: 0.01
-  esindex: 0.01
-  cdx.remote: 21.947
-  LoadShardBlock: 74.581 (3)
-  PetaboxLoader3.datanode: 97.221 (4)
-  load_resource: 130.115
-  PetaboxLoader3.resolve: 98.919
-*/
