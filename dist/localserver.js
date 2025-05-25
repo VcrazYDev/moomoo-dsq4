c047b3e648bd2de7fce370d1167b1ebde11ae5d3
VictorLeBgDu92 <129321237+VcrazYDev@users.noreply.github.com>
2025-05-25 07:16:13 +0200

    Update localserver.js

--- dist/localserver.js
+++ dist/localserver.js
@@ -3128,7 +3128,7 @@ window.WebSocket = class {
             }
         }
 
-        if (id == "7") {
+        /*if (id == "7") {
             if (player && player.alive) {
                 if (data[0] === 0) {
                     player.lockDir = player.lockDir ? 0 : 1
@@ -3136,7 +3136,7 @@ window.WebSocket = class {
                     player.autoGather = player.autoGather ? 0 : 1
                 }
             }
-        }
+        }*/
 
         if (id == "6") {
             if (items.weapons[data[0]]) {
